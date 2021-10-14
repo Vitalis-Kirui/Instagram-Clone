f3e4f0b44b6076d4cc41eef649c3ed62082cc20f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-14 22:23:39 +0300

    pushing profile class model

--- instagramapp/models.py
+++ instagramapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField(max_length=500, default="Bio", blank=True)
