2a466248f4270c26f70779962f2ed46384f72f6b
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-14 20:57:19 +0300

    adding urls.py file to app and a homepage view function

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def homepage(request):
+    """
+    View function to display homepage content
+    """
+    
