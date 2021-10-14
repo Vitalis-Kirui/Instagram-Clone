fea815e764d8476ccb690b5bd1976740049a25e7
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-14 22:44:19 +0300

    pushing successful test on profile class model

--- instagramapp/tests.py
+++ instagramapp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Comment, Profile, Image
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    """Test for the profile model class"""
+    def setUp(self):
+        self.user = User(username='')
+        self.user.save()
+
+        self.profile = Profile(id=75 ,profile_pic='profile-pic.jpg', bio='this is a setup to test profile',
+                                    user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_profile(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) > 0)
+
+    def test_update_profile(self):
+        self.profile.save_profile()
+        self.profile.update_profile(self.profile.user_id)
+        self.profile.save_profile()
+        self.assertTrue(Profile,self.profile.user)
