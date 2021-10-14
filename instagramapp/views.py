a5a27dad6f03dc137d937d856ba665561dd22284
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-14 21:03:38 +0300

    pushing the first template

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 
 # Create your views here.
 def homepage(request):
     """
     View function to display homepage content
     """
-    
+    return render(request, 'home.html')
