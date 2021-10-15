0176d57f39a9eb229b63459baeae804ac74659e8
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-15 14:03:22 +0300

    pushing search view function

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404
 from django.forms.widgets import DateTimeInput
+from django.http.response import HttpResponse, HttpResponseRedirect
 from instagramapp.models import Comment, Image, Profile
 
 # Create your views here.
@@ -13,3 +13,15 @@ def homepage(request):
     comment = Comment.objects.all()
 
     return render(request, 'all-templates/home.html',{"posts":posts,"profile":profile,"comment":comment})
+
+def search(request): 
+    if 'profile' in request.GET and request.GET['profile']:
+        user = request.GET.get("profile")
+
+        print(user)
+        results = Profile.search_profile(user)
+        message = f'profile'
+        return render(request, 'all-templates/search.html',{'profiles': results,'message': message})
+    else:
+        message = "You haven't entered anything to search. Please enter a user profile to search."
+    return render(request, 'all-templates/search.html', {'message': message})
