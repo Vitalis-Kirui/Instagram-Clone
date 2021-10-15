a98c6be9ba190b97d168c9a19f27cc07abf117c4
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-15 08:56:17 +0300

    pushing home html file to display image posted

--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
+from django.forms.widgets import DateTimeInput
+from instagramapp.models import Comment, Image, Profile
 
 # Create your views here.
 def homepage(request):
